a0cfadd3c798a3e116a556fbc325b00fb5e4a240
vholik <1viktorholik@gmail.com>
2024-05-07 15:01:57 +0200

    refactor: Update class-validator usage in AdminPostProductsReq

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,10 @@
 import { AdminPostProductsReq as MedusaAdminPostProductsReq } from '@medusajs/medusa/dist/api/routes/admin/products/create-product';
 import { AdminPostProductsProductReq as MedusaAdminPostProductsProductReq } from '@medusajs/medusa/dist/api/routes/admin/products/update-product';
-import { IsString, ArrayMaxSize, IsArray, IsOptional } from 'class-validator';
+import { IsString, ArrayMaxSize, IsOptional } from 'class-validator';
 import { registerOverriddenValidators } from '@medusajs/medusa';
 
 class AdminPostProductsReq extends MedusaAdminPostProductsReq {
-	@IsString()
-	@IsArray()
+	@IsString({ each: true })
 	@ArrayMaxSize(10)
 	@IsOptional()
 	shipping_options?: string[];
