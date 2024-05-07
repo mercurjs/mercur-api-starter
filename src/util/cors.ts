3250ba810cb9d4f2ba54b5aa179894870961f79f
vholik <1viktorholik@gmail.com>
2024-05-07 10:19:56 +0200

    refactor: Update CORS configuration for admin and store

--- src/util/cors.ts
+++ src/util/cors.ts
@@ -1,9 +1,9 @@
 export const adminCors = {
-	origin: process.env.ADMIN_CORS.split(','),
+	origin: process.env.ADMIN_CORS?.split(','),
 	credentials: true,
 };
 
 export const storeCors = {
-	origin: process.env.STORE_CORS.split(','),
+	origin: process.env.STORE_CORS?.split(','),
 	credentials: true,
 };
